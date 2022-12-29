65c58847a5517f336bf68b7e92f9e6612ca96224
Mikhail-hash <81635000+Mikhail-hash@users.noreply.github.com>
2022-12-29 19:07:45 +0300

    Add files via upload

---
+++ Стандартные функции с символами и строками/Стандартные функции с символами и строками.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+#include <clocale>
+using namespace std;
+int main()
+{
+    setlocale(LC_CTYPE, "rus");
+    char s;
+    char a[] = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+    cin >> s;
+    for (int i = 1; i <= strlen(s); i++) {
+        if (s[i - 1] = " ") {
+            for (int j = 0; j <= strlen(a); j++) {
+                if (s[i] == a[j] && j > 32 && j < 66) {
+                    s[i] = a[j - 33];
+                }
+                else {
+                    if (s[i] == a[j] && j < 33) {
+                        continue
+                    }
+                }
+            }
+        }
+    }
+}
