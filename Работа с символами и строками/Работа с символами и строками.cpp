82765b017e44c6462cb22b506a8f83daaa3f8444
Mikhail-hash <81635000+Mikhail-hash@users.noreply.github.com>
2022-12-29 18:54:49 +0300

    Add files via upload

---
+++ Работа с символами и строками/Работа с символами и строками.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+#include <clocale>
+using namespace std;
+int main()
+{
+    setlocale(LC_CTYPE, "rus");
+    int k, i, j;
+    char s;
+    char a[] = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+    cin >> k;
+    cin >> s;
+    for (int i = 0; i <= strlen(s); i++) {
+        for (int j = 0; j <= strlen(a); j++) {
+            if (s[i] == a[j]) {
+                cout << a[j + k];
+            }
+        }
+    }
+}
