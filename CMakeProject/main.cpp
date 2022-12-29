f4412bbadf259c92a30174ea85a816bac60b90fa
Mikhail-hash <81635000+Mikhail-hash@users.noreply.github.com>
2022-12-29 19:19:54 +0300

    Add files via upload

---
+++ CMakeProject/main.cpp
@@ -0,0 +1,8 @@
+﻿// CMakeProject.cpp: определяет точку входа для приложения.
+//
+#include <iostream>
+#include "CMakeProject.h"
+int main(int argc, char** argv){
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
