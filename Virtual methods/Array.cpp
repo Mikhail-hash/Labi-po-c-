2ea7e0614dc7d6acbcd874d10b5fa6dc4143e4b3
Mikhail-hash <81635000+Mikhail-hash@users.noreply.github.com>
2023-02-01 02:53:02 +0300

    Update Array.cpp
    15. Создать класс BitString (Класс для работы с битовыми строками не более чем из 100 бит.
    Битовая строка должна быть представлена массивом типа unsigned char, каждый элемент которого принимает значение 0 или 1.)
    и класс String (Класс для работы со строками, аналогичными строками Turbo Pascal (строка представляется как массив 255 байт, длина - в первом байте).
    Максимальный размер строки должен задаваться.).

--- Virtual methods/Array.cpp
+++ Virtual methods/Array.cpp
@@ -28,7 +28,7 @@ Array Array::operator + (Array& in_arr) {
 	int max_len = max(length(), in_arr.length());
 	Array ret(max_len);
 
-	int min_len = min(length(), in_arr.length());
+	int min_len = min(length(), in_arr.length()); 
 	for (int index = 0; index < max_len; index++) {
 		ret[index] = (index < min_len) ? arr[index] + in_arr.arr[index] : ((min_len == length()) ? in_arr.arr[index] : arr[index]);
 	}
