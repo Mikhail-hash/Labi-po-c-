afdd7bcb8625f1ecdc2f72994dcfffd9a1d036bb
Mikhail-hash <81635000+Mikhail-hash@users.noreply.github.com>
2023-02-02 01:42:32 +0300

    Add files via upload

---
+++ Inheritance/Inheritance.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include "Number.h"
+#include "Real.h"
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    float Real a = 4;
+
+    float Number n;
+
+    float Real z = -2;
+
+    return 0;
+}
---
+++ Inheritance/Real.h
@@ -0,0 +1,15 @@
+#include "Number.h"
+#include "Real.cpp"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <stdlib.h>
+#include <cmath>
+using namespace std;
+;
+float Real a(4)
+cout << a.getLn();
+
+float Real z(-2);
+cout << z.getLn();
